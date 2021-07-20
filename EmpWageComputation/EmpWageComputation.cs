e5adf5b4bb84f40cef8186db00a6ccc8d12fb79e
bharathmighty248 <bharathmighty248@gmail.com>
2021-07-20 17:16:06 +0530

    [Bharath] Add: Monthly Wage Calculation Implemented

--- EmpWageComputation/EmpWageComputation.cs
+++ EmpWageComputation/EmpWageComputation.cs
@@ -9,27 +9,35 @@ namespace EmpWageComputation
             //Constants
             const int IS_FULLTIME = 1;
             const int IS_PARTTIME = 2;
+            const int NUM_OF_WORKING_DAYS = 20;
             const int WAGE_PER_HR = 20;
             //Variables
             int empHrs = 0;
-            int empWage = 0;
+            int dailyWage = 0;
+            int monthlyWage = 0;
+
             Random random = new Random();
             //Computation
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            for (int day=1; day<=NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_FULLTIME:
-                    empHrs = 8;
-                    break;
-                case IS_PARTTIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULLTIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PARTTIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                dailyWage = empHrs * WAGE_PER_HR;
+                monthlyWage = monthlyWage + dailyWage;
             }
-            empWage = empHrs * WAGE_PER_HR;
-            Console.WriteLine("Emp Wage: " + empWage);
+            
+            Console.WriteLine("Total Monthly Wage: " + monthlyWage);
         }
     }
 }

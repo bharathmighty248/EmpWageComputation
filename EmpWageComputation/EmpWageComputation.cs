03997df923ab9a476173ce9c1e0ceb795e9f2784
bharathmighty248 <bharathmighty248@gmail.com>
2021-07-20 15:21:50 +0530

    [Bharath] Welcome Message

---
+++ EmpWageComputation/EmpWageComputation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWageComputation
+{
+    class EmpWageComputation
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}

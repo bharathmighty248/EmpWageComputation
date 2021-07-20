e1d05b57795810ddf53d71c16129c27685a845fe
bharathmighty248 <bharathmighty248@gmail.com>
2021-07-20 16:03:40 +0530

    [Bharath] Add: Wage Calculation Implemented

--- EmpWageComputation/EmpWageComputation.cs
+++ EmpWageComputation/EmpWageComputation.cs
@@ -8,13 +8,19 @@ namespace EmpWageComputation
         {
             //Constants
             const int IS_PRESENT = 1;
+            const int WAGE_PER_HR = 20;
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_PRESENT)
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             else
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+            empWage = empHrs * WAGE_PER_HR;
+            Console.WriteLine("Emp Wage: " + empWage);
         }
     }
 }

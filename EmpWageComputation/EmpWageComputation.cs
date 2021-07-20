56cab965a718600e542f940ab8c4582e1ac397d2
bharathmighty248 <bharathmighty248@gmail.com>
2021-07-20 16:16:16 +0530

    [Bharath] Add: Parttime EmpWage Implemented

--- EmpWageComputation/EmpWageComputation.cs
+++ EmpWageComputation/EmpWageComputation.cs
@@ -7,16 +7,19 @@ namespace EmpWageComputation
         static void Main(string[] args)
         {
             //Constants
-            const int IS_PRESENT = 1;
+            const int IS_FULLTIME = 1;
+            const int IS_PARTTIME = 2;
             const int WAGE_PER_HR = 20;
             //Variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             //Computation
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_PRESENT)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_FULLTIME)
                 empHrs = 8;
+            else if (empCheck == IS_PARTTIME)
+                empHrs = 4;
             else
                 empHrs = 0;
             empWage = empHrs * WAGE_PER_HR;

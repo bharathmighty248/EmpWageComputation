4093b99ffb55925e9fa949a511e8f210a7883f73
bharathmighty248 <bharathmighty248@gmail.com>
2021-07-20 15:44:29 +0530

    [Bharath] Add: EmpAttendanceMonitoring

--- EmpWageComputation/EmpWageComputation.cs
+++ EmpWageComputation/EmpWageComputation.cs
@@ -6,7 +6,15 @@ namespace EmpWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            //Constants
+            const int IS_PRESENT = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_PRESENT)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
         }
     }
 }
